55f4b51e15067fd814566c07fae7fe3d3b7df75c
einaur <eanorway@gmail.com>
2022-08-16 15:51:57 +0200

    added transition moments

--- ci_singles/cis.py
+++ ci_singles/cis.py
@@ -16,20 +16,61 @@ class CIS:
         self.u = system.u
         self.f = system.construct_fock_matrix(system.h, system.u)
 
+    def compute_one_body_density_matrix_IJ(self, I, J):
+        np = self.np
+
+        Iocc = np.eye(self.system.n)
+
+        occ = self.system.o
+        virt = self.system.v
+
+        nocc = occ.stop
+        nvirt = virt.stop - nocc
+
+        CI0 = self.C[0, I]
+        CJ0 = self.C[0, J]
+        CI_ai = self.C[1:, I].reshape(nvirt, nocc)
+        CJ_ai = self.C[1:, J].reshape(nvirt, nocc)
+
+        rho = np.zeros(self.system.h.shape, dtype=np.complex128)
+
+        rho[occ, occ] = 2 * Iocc * (
+            CI0 * CJ0.conj() + np.einsum("ia,ai->", CI_ai.T.conj(), CJ_ai)
+        ) - np.einsum("ja, ai->ji", CI_ai.conj().T, CJ_ai)
+
+        rho[virt, occ] = np.sqrt(2) * CI0.conj() * CJ_ai
+        rho[occ, virt] = np.sqrt(2) * CJ0 * CI_ai.T.conj()
+        rho[virt, virt] = np.einsum("ia,bi->ba", CI_ai.T.conj(), CJ_ai)
+
+        return rho
+
+    def compute_transition_dipole_moment(self, I, J):
+        r = self.system.position
+        rho = self.compute_one_body_density_matrix_IJ(I, J)
+        return self.np.einsum("qp,ipq->i", rho, r)
+
+    def compute_transition_moment(self, I, J, mat):
+        rho = self.compute_one_body_density_matrix_IJ(I, J)
+        if len(mat.shape) == 2:
+            return self.np.einsum("qp,pq->", rho, mat)
+        else:
+            return self.np.einsum("qp,ipq->i", rho, mat)
+
     def compute_ground_state(self):
+        np = self.np
 
         occ = self.o
         virt = self.v
 
-        e_ref = 2 * self.np.einsum("ii->", self.h[occ, occ])
-        e_ref += 2 * self.np.einsum("ijij->", self.u[occ, occ, occ, occ])
-        e_ref -= self.np.einsum("ijji->", self.u[occ, occ, occ, occ])
+        e_ref = 2 * np.einsum("ii->", self.h[occ, occ])
+        e_ref += 2 * np.einsum("ijij->", self.u[occ, occ, occ, occ])
+        e_ref -= np.einsum("ijji->", self.u[occ, occ, occ, occ])
 
-        self.hamiltonian = self.np.zeros((self.num_states, self.num_states))
+        self.hamiltonian = np.zeros((self.num_states, self.num_states))
 
         self.hamiltonian[0, 0] = e_ref
-        self.hamiltonian[0, 1:] = self.np.sqrt(2) * self.f[virt, occ].ravel()
-        self.hamiltonian[1:, 0] = self.np.sqrt(2) * self.f[occ, virt].ravel()
+        self.hamiltonian[0, 1:] = np.sqrt(2) * self.f[virt, occ].ravel()
+        self.hamiltonian[1:, 0] = np.sqrt(2) * self.f[occ, virt].ravel()
 
         Iocc = self.np.eye(self.n)
         Ivirt = self.np.eye(self.m)
